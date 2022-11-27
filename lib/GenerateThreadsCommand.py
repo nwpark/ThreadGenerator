d49c7c1fa3bab4caaddb593f41a3b9dd2968273e
Nick Park <nick.wpa@gmail.com>
2022-11-27 12:23:23 +0000

    refactor instance variables to be protected

--- lib/GenerateThreadsCommand.py
+++ lib/GenerateThreadsCommand.py
@@ -8,35 +8,35 @@ from .common.Common import ui, printTrace, resourceFolder
 
 class GenerateThreadsCommand:
     def __init__(self):
-        self.commandCreatedHandler = self._CommandCreatedHandler()
-        self.commandDefinition = ui.commandDefinitions.itemById('ThreadGenerator')
-        if not self.commandDefinition:
-            self.commandDefinition = ui.commandDefinitions.addButtonDefinition('ThreadGenerator', 'Generate Thread',
-                                                                               'Generates threads.', resourceFolder)
-        self.commandDefinition.commandCreated.add(self.commandCreatedHandler)
+        self._commandCreatedHandler = self._CommandCreatedHandler()
+        self._commandDefinition = ui.commandDefinitions.itemById('ThreadGenerator')
+        if not self._commandDefinition:
+            self._commandDefinition = ui.commandDefinitions.addButtonDefinition('ThreadGenerator', 'Generate Thread',
+                                                                                'Generates threads.', resourceFolder)
+        self._commandDefinition.commandCreated.add(self._commandCreatedHandler)
 
     def execute(self):
         inputs = NamedValues.create()
-        self.commandDefinition.execute(inputs)
+        self._commandDefinition.execute(inputs)
 
     class _CommandCreatedHandler(CommandCreatedEventHandler):
         def __init__(self):
             super().__init__()
-            self.onExecuteHandler = None
-            self.onExecutePreviewHandler = None
-            self.onDestroyHandler = None
+            self._onExecuteHandler = None
+            self._onExecutePreviewHandler = None
+            self._onDestroyHandler = None
 
         def notify(self, args: CommandCreatedEventArgs):
             try:
                 cmd = args.command
                 cmd.isRepeatable = False
 
-                self.onExecuteHandler = OnExecuteHandler()
-                self.onExecutePreviewHandler = OnExecuteHandler()
-                self.onDestroyHandler = OnDestroyHandler()
-                cmd.execute.add(self.onExecuteHandler)
-                cmd.executePreview.add(self.onExecutePreviewHandler)
-                cmd.destroy.add(self.onDestroyHandler)
+                self._onExecuteHandler = OnExecuteHandler()
+                self._onExecutePreviewHandler = OnExecuteHandler()
+                self._onDestroyHandler = OnDestroyHandler()
+                cmd.execute.add(self._onExecuteHandler)
+                cmd.executePreview.add(self._onExecutePreviewHandler)
+                cmd.destroy.add(self._onDestroyHandler)
 
                 for userParameter in UserParameters.asList():
                     userParameter.addToCommandInputs(cmd.commandInputs)
